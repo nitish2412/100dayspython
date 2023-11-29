d93053c69bc45a3191890fed3f46d69caef57c9a
Nitish Kumar <nitishcs007@gmail.com>
2023-11-29 17:06:29 +0530

    day2 code added

---
+++ day2/age_left.py
@@ -0,0 +1,7 @@
+age = input()
+# 🚨 Don't change the code above 👆
+# Write your code below this line 👇
+age=int(age)
+left_year=90-age
+week_left=left_year*52
+print(f"You have {week_left} weeks left.")
---
+++ day2/test.py
@@ -0,0 +1,5 @@
+#Data Types
+score=0
+height=80
+isWinning=True
+print(f"Your score is {score}, height is: {height}, is winning: {isWinning}")
